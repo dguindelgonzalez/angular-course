919641cc0e98d8e605dac82e039e0d4a9bb4cf3d
Daniel Guindel Gonzalez <daniel.guindel_gonzalez@contractors.roche.com>
2021-03-14 09:39:35 +0100

    bindings

---
+++ MyFirstApp/src/app/saludo/saludo.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  mensaje: string;
+  identifParrafo: string;
+  identifInput: string;
+
+  constructor() {
+    this.mensaje = 'Un saludo a todos';
+    this.identifParrafo ='parrafoPrincipal';
+    this.identifInput = 'text';
+
+    setTimeout(() => {
+      this.mensaje = 'Otro mensaje diferente';
+      this.identifParrafo = 'main';
+      this.identifInput = 'password';
+
+    }, 3000);
+   }
+
+  ngOnInit(): void {
+  }
+
+  mostrarSaludo(): string{
+    return 'Saludo desde el método';
+  }
+
+}
