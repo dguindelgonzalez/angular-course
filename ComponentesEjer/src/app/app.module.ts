8bd8a024d08c4cdb6417d00fa6a1b930bef8daed
Daniel Guindel Gonzalez <daniel.guindel_gonzalez@contractors.roche.com>
2021-03-15 18:07:13 +0100

    33 calculadora

--- ComponentesEjer/src/app/app.module.ts
+++ ComponentesEjer/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SumarComponent } from './components/sumar/sumar.component';
 import { CronometroComponent } from './components/cronometro/cronometro.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SumarComponent,
-    CronometroComponent
+    CronometroComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule
---
+++ ComponentesEjer/src/app/components/calculadora/calculadora.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+  input: string;
+  resultFlag: boolean;
+
+  constructor() {
+    this.input = '';
+    this.resultFlag = false;
+   }
+
+  ngOnInit(): void {
+  }
+
+  onClick($event){
+    if(this.resultFlag){
+      this.resultFlag = false;
+      this.input = '';
+    }
+
+    this.input += $event.target.innerText;
+  }
+
+  onEquals(){
+    this.input = eval(this.input);
+    this.resultFlag = true;
+  }
+}
