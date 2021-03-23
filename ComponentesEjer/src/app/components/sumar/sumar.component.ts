e2847cbb5c715e3140ea14d49693b12ab8a77bb2
Daniel Guindel Gonzalez <daniel.guindel_gonzalez@contractors.roche.com>
2021-03-23 11:34:10 +0100

    37 modificacion componente sumar

--- ComponentesEjer/src/app/components/sumar/sumar.component.ts
+++ ComponentesEjer/src/app/components/sumar/sumar.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sumar',
   templateUrl: './sumar.component.html',
-  styleUrls: ['./sumar.component.css']
+  styleUrls: ['./sumar.component.css'],
 })
 export class SumarComponent implements OnInit {
-
-  sumador1: number;
-  sumador2: number;
+  @Input() sumador1: number;
+  @Input() sumador2: number;
   total: number;
 
   constructor() {
     this.sumador1 = 56;
     this.sumador2 = 44;
     this.total = 0;
-   }
-
-  ngOnInit(): void {
   }
 
-  suma(){
+  ngOnInit(): void {}
+
+  suma() {
     this.total = this.sumador1 + this.sumador2;
   }
 }
